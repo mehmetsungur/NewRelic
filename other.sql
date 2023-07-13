ca02eef509292f0e59d75930138560470ede74fd
mehmetsungur <mehmetsungur90@gmail.com>
2023-07-13 10:23:20 +0300

    update

--- other.sql
+++ other.sql
@@ -1,1 +1,5 @@
 SELECT count(`apm.service.external.host.duration`) FROM Metric WHERE appName LIKE '%' SINCE LAST WEEK FACET external.host limit max
+
+SELECT uniqueCount(httpResponseCode) 
+FROM Transaction 
+WHERE appId = 174468149 AND request.uri= '/api/Funnel/SendEmail' AND httpResponseCode = 200
